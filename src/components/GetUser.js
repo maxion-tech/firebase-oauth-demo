de2cd5ac9fc591d5c41089c6d7188f766b98de5f
passon <passon@maxion.tech>
2023-08-03 11:01:26 +0700

    fix eslint issue

--- src/components/GetUser.js
+++ src/components/GetUser.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 // Move the fetchData function outside of the component
