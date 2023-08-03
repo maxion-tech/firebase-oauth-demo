3f8c56bc5da69288a5da1c385eb540f68bffd6aa
passon <passon@maxion.tech>
2023-08-03 10:48:30 +0700

    fix bug client keep fetching data from server all the time

--- src/components/GetUser.js
+++ src/components/GetUser.js
@@ -1,31 +1,32 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 
+// Move the fetchData function outside of the component
+const fetchData = async (token, setUser) => {
+  try {
+    const response = await axios.get('http://localhost:5000/user/user-data', {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+    console.log('fetching data..');
+    console.log("user data = ", response.data.data);
+    setUser(response.data.data); // Update state with the fetched user data
+    console.log('user = ', response.data.data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
 export default function GetUser({ token }) {
   const [user, setUser] = useState({});
 
-  const fetchData = useCallback(async (token) => {
-    try {
-      const response = await axios.get('http://localhost:5000/user/user-data', {
-        headers: {
-          Authorization: 'Bearer ' + token,
-        },
-      });
-      console.log('fetching data..');
-      console.log("user data = ", response.data.data);
-      setUser(response.data.data); // Update state with the fetched user data
-      console.log('user = ', user);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }, [user]); // Empty dependency array since fetchData doesn't depend on any props or state
-
-  // Run fetchData only once during the initial mount
+  // Use the fetchData function from outside the component
   useEffect(() => {
     if (token) {
-      fetchData(token);
+      fetchData(token, setUser);
     }
-  }, [fetchData, token]); // Empty dependency array to run the effect only once
+  }, [token]);
 
   return (
     <div>
